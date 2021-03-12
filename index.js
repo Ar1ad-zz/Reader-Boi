657212d97891c9840e7e8d74a2d119c8f26fc78d
Ar1ad <sora4gamers@gmail.com>
2021-03-12 10:55:54 +0100

    changed token read again

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ const config = require('./config.json');
 const dotenv = require('dotenv');
 const aws = require('aws-sdk');
 
-let s3 = new aws.S3({
-    Token: process.env.S3_TOKEN
-});
 dotenv.config()
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(process.env.TOKEN || Token)
+client.login(process.env.TOKEN)
 client.on('message', message => {
     if (message.content === '${prefix}ping')  message.channel.send('pong.');
 });
