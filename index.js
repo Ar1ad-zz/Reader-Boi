fc71e662c8df1f50ea2354630931b760b853596a
Ar1ad <sora4gamers@gmail.com>
2021-03-15 08:35:08 +0100

    hmm

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const client = new Discord.Client();
 const config = require('./config.json');
 const dotenv = require('dotenv');
 
+const Pg = require('pg');
+
+const pgclient = new Pg.Client({
+    connectionString: process.env.DATABES_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+});
+
+pgclient.connect();
+
 dotenv.config()
 
 client.once('ready', () => {
